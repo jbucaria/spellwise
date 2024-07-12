c37e05b08e527f0addb1b149f6f7e4868b5af640
jbucaria <157171759+jbucaria@users.noreply.github.com>
2024-07-12 17:45:39 -0400

    intergrated main.js into index.js

--- public/js/index.js
+++ public/js/index.js
@@ -3,15 +3,29 @@
 import '@babel/polyfill';
 import { login, logout } from './login';
 import { updateSettings } from './updateSettings';
+import { createCards, cardsEl, updateCurrentText } from './main';
 
 // DOM ELEMENTS
+// Login/Logout
 const loginForm = document.querySelector('.form--login');
 const logOutBtn = document.querySelector('.nav__el--logout');
 const userDataForm = document.querySelector('.form-user-data');
 const userPasswordForm = document.querySelector('.form-user-password');
 
-// DELEGATION
+//Cards
 
+const prevBtn = document.getElementById('prev');
+const nextBtn = document.getElementById('next');
+const cardsContainer = document.getElementById('cards-container');
+const showBtn = document.getElementById('show');
+const hideBtn = document.getElementById('hide');
+const questionEl = document.getElementById('question');
+const answerEl = document.getElementById('answer');
+const addCardBtn = document.getElementById('add-card');
+const addContainer = document.getElementById('add-container');
+
+// DELEGATION
+// Login Form
 if (loginForm)
   loginForm.addEventListener('submit', e => {
     e.preventDefault();
@@ -48,3 +62,55 @@ if (userPasswordForm)
     document.getElementById('password').value = '';
     document.getElementById('password-confirm').value = '';
   });
+
+//Main
+// Next button
+
+if (cardsContainer) {
+  let currentActiveCard = 0;
+  createCards();
+
+  nextBtn.addEventListener('click', () => {
+    cardsEl[currentActiveCard].className = 'card left';
+    currentActiveCard += 1;
+    if (currentActiveCard > cardsEl.length - 1) {
+      currentActiveCard = cardsEl.length - 1;
+    }
+    cardsEl[currentActiveCard].className = 'card active';
+    // updateCurrentText();
+    console.log(currentActiveCard);
+  });
+
+  // Prev button
+  prevBtn.addEventListener('click', () => {
+    cardsEl[currentActiveCard].className = 'card right';
+    currentActiveCard -= 1;
+    if (currentActiveCard < 0) {
+      currentActiveCard = 0;
+    }
+    cardsEl[currentActiveCard].className = 'card active';
+    updateCurrentText();
+  });
+
+  // Show add container
+  showBtn.addEventListener('click', () => addContainer.classList.add('show'));
+  // Hide add container
+  hideBtn.addEventListener('click', () =>
+    addContainer.classList.remove('show'),
+  );
+
+  // Add new card
+  addCardBtn.addEventListener('click', () => {
+    const question = questionEl.value;
+    const answer = answerEl.value;
+    if (question.trim() && answer.trim()) {
+      const newCard = { question, answer };
+      createCard(newCard);
+      questionEl.value = '';
+      answerEl.value = '';
+      addContainer.classList.remove('show');
+      cardsData.push(newCard);
+      setCardsData(cardsData);
+    }
+  });
+}
--- public/js/main.js
+++ public/js/main.js
@@ -1,23 +1,10 @@
-const cardsContainer = document.getElementById('cards-container');
-const prevBtn = document.getElementById('prev');
-const nextBtn = document.getElementById('next');
-const currentEl = document.getElementById('current');
-const showBtn = document.getElementById('show');
-const hideBtn = document.getElementById('hide');
-const questionEl = document.getElementById('question');
-const answerEl = document.getElementById('answer');
-const addCardBtn = document.getElementById('add-card');
-const addContainer = document.getElementById('add-container');
-
 // Keep track of current card
-let currentActiveCard = 0;
 
-// Store DOM cards
-const cardsEl = [];
+export const cardsEl = [];
 
 const cardsData = [
   {
-    question: 'What must a variable begin with?',
+    question: 'What must a variable?',
     answer: 'A letter, $ or _',
   },
   {
@@ -28,15 +15,27 @@ const cardsData = [
     question: 'Example of Case Sensitive Variable',
     answer: 'thisIsAVariable',
   },
+  {
+    question: 'Example of Case Sensitive Variable',
+    answer: 'thisIsAVariable',
+  },
 ];
 
 // Create all cards
-function createCards() {
+export function createCards() {
   cardsData.forEach((data, index) => createCard(data, index));
+}
+
+export function updateCurrentText() {
+  let currentActiveCard = 0;
+  const currentEl = document.getElementById('current');
+  currentEl.innerText = `${currentActiveCard + 2}/${cardsEl.length}`;
 }
 
 // Create a single card in DOM
 function createCard(data, index) {
+  const cardsContainer = document.getElementById('cards-container');
+
   const card = document.createElement('div');
   card.classList.add('card');
 
@@ -59,9 +58,7 @@ function createCard(data, index) {
 </div>
   `;
   // Show number of cards
-  function updateCurrentText() {
-    currentEl.innerText = `${currentActiveCard + 1}/${cardsEl.length}`;
-  }
+  updateCurrentText();
 
   card.addEventListener('click', () => card.classList.toggle('show-answer'));
 
@@ -72,62 +69,3 @@ function createCard(data, index) {
 
   updateCurrentText();
 }
-
-createCards();
-
-// Event listeners
-
-// Next button
-nextBtn.addEventListener('click', () => {
-  cardsEl[currentActiveCard].className = 'card left';
-
-  currentActiveCard += 1;
-
-  if (currentActiveCard > cardsEl.length - 1) {
-    currentActiveCard = cardsEl.length - 1;
-  }
-
-  cardsEl[currentActiveCard].className = 'card active';
-
-  updateCurrentText();
-});
-
-// Prev button
-prevBtn.addEventListener('click', () => {
-  cardsEl[currentActiveCard].className = 'card right';
-
-  currentActiveCard -= 1;
-
-  if (currentActiveCard < 0) {
-    currentActiveCard = 0;
-  }
-
-  cardsEl[currentActiveCard].className = 'card active';
-
-  updateCurrentText();
-});
-
-// Show add container
-showBtn.addEventListener('click', () => addContainer.classList.add('show'));
-// Hide add container
-hideBtn.addEventListener('click', () => addContainer.classList.remove('show'));
-
-// Add new card
-addCardBtn.addEventListener('click', () => {
-  const question = questionEl.value;
-  const answer = answerEl.value;
-
-  if (question.trim() && answer.trim()) {
-    const newCard = { question, answer };
-
-    createCard(newCard);
-
-    questionEl.value = '';
-    answerEl.value = '';
-
-    addContainer.classList.remove('show');
-
-    cardsData.push(newCard);
-    setCardsData(cardsData);
-  }
-});
